b699228a7b14ea9b69f301f423df57bbae0702cd
AnnabellaZhang <zhangrb12@qq.com>
2016-10-04 14:15:41 +0800

    done Fibonacci

--- HW2/Fibonacci.py
+++ HW2/Fibonacci.py
@@ -17,8 +17,7 @@ def bu_f(n):
         i += 1
     return f1
 
-
-#分治法求斐波那契数
+#朴素递归法求斐波那契数
 def re_f(n):
     if n == 0:
         return 0
@@ -26,23 +25,74 @@ def re_f(n):
         return 1
     return re_f(n-1)+re_f(n-2)
 
+#递归平方法求斐波那契
+def rs_f(n):
+    # 矩阵相乘
+    def mul_metrix((a, b, c, d), (e, f, g, h)):
+        return (a * e + b * g, a * f + b * h, c * e + d * g, c * f + d * h)
+    #矩阵n次幂
+    def metrix_pow(n, (a, b, c, d)):
+        if n == 0:
+            return (1, 0, 0, 1)
+        if n == 1:
+            return (a, b, c, d)
+        if n%2 == 0:
+            return mul_metrix(metrix_pow(n/ 2, (a, b, c, d)), metrix_pow(n/2, (a, b, c, d)))
+        else:
+            return mul_metrix(metrix_pow((n+1) / 2, (a, b, c, d)), metrix_pow((n-1)/2, (a, b, c, d)))
+    return metrix_pow(n,(1,1,1,0))[1]
+
+#朴素递归法求斐波那契
+def nrs_f(n):
+    #求x的n次幂
+    def a_pow(a, n):
+        if n == 0:
+            return 1
+        if n == 1:
+            return a
+        if n % 2 == 0:
+            return a_pow(a, n / 2) * a_pow(a, n / 2)
+        else:
+            return a_pow(a, (n + 1) / 2) * a_pow(a, (n - 1) / 2)
+    a = (1 + math.sqrt(5))/2
+    return int(round(a_pow(a,n)/math.sqrt(5)))
+
 
 k = 1
-while k < 6:
-    num = math.pow(2, k)
+while k < 20:
+    #num = 25 + 2*k
+    num = pow(2,k)
     print num
-    t0 = time.time()
+    t0 = time.clock()
     result = bu_f(num)
     print result
-    t1 = time.time()
-    print ("Total time running bu_f(%s) = %s : %s seconds" %
-           (str(num), str(result), str(t1 - t0))
+    t1 = time.clock()
+    print ("Total time running bu_f(%s) : %s seconds" %
+           (str(num),  str(t1 - t0))
            )
-    t0 = time.time()
+    '''
+    t0 = time.clock()
     result = re_f(num)
     print result
-    t1 = time.time()
-    print ("Total time running re_f(%s) = %s : %s seconds" %
-           (str(num), str(result), str(t1 - t0))
+    t1 = time.clock()
+    print ("Total time running re_f(%s) : %s seconds" %
+           (str(num), str(t1 - t0))
            )
+    '''
+    t0 = time.clock()
+    result = rs_f(num)
+    print result
+    t1 = time.clock()
+    print ("Total time running rs_f(%s) : %s seconds" %
+           (str(num),  str(t1 - t0))
+           )
+    '''
+    t0 = time.clock()
+    result = nrs_f(num)
+    print result
+    t1 = time.clock()
+    print ("Total time running nrs_f(%s) : %s seconds" %
+           (str(num),  str(t1 - t0))
+           )
+    '''
     k += 1
--- HW2/hello.py
+++ HW2/hello.py
@@ -1,10 +1,29 @@
 # -*- coding: utf-8 -*-
+import math
+import datetime
 
-#矩阵相乘
-def metrix_pow(n,(a,b,c,d)):
-    if n == 1:
-        return (a,b,c,d)
-    if n == 2:
-        return (a*a+b*c,a*b+b*d,a*c+c*d,b*c+d*d)
 
-print metrix_pow(2,(1,1,1,0))
+
+#朴素递归法求斐波那契
+def nrs_f(n):
+    #求x的n次幂
+    def a_pow(a, n):
+        if n == 0:
+            return 1
+        if n == 1:
+            return a
+        if n % 2 == 0:
+            return a_pow(a, n / 2) * a_pow(a, n / 2)
+        else:
+            return a_pow(a, (n + 1) / 2) * a_pow(a, (n - 1) / 2)
+    a = (1 + math.sqrt(5))/2
+    return int(round(a_pow(a,n)/math.sqrt(5)))
+
+
+
+
+
+n = 0
+while n < 10:
+    print nrs_f(n)
+    n += 1
